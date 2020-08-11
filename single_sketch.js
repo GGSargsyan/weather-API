2a12c4e9c5ace14ea2a9ea24e202534df2e746f7
GGSargsyan <grigor.sargsyan@yahoo.com>
2020-08-10 22:56:00 -0700

    Added API calls

--- single_sketch.js
+++ single_sketch.js
@@ -1,13 +1,20 @@
 // Single-sketch example
+var weather;
 
 function setup (){
-  createCanvas (800, 600);
+  createCanvas (600, 400);
+  loadJSON('http://api.openweathermap.org/data/2.5/weather?id=524901&appid=ca44a6aad9418c3af43cff3f34fa6819', gotData);
 }
+
+function gotData(data){
+  weather = data;
+}
+
 
 function draw(){
   background(100);
-  fill(255);
-  noStroke();
-  rectMode(CENTER);
-  rect(mouseX, mouseY, 50, 50);
+  if(weather){
+    ellipse(400, 200, weather.main.temp, weather.main.temp);
+    ellipse(100, 200, weather.main.humidity, weather.main.humidity);
+  }
 }
