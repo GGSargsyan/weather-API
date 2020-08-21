eb748c450f441c15bde0c4c4fc03b23f3b616c73
GGSargsyan <grigor.sargsyan@yahoo.com>
2020-08-21 12:45:21 -0700

    added interactive button

--- single_sketch.js
+++ single_sketch.js
@@ -1,15 +1,32 @@
 // Single-sketch example
 var weather;
+var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+//var city = 'San Diego';
+var units = '&units=metric';
+
+var input;
 
 function setup (){
   createCanvas (600, 400);
-  loadJSON('http://api.openweathermap.org/data/2.5/weather?id=524901&appid=ca44a6aad9418c3af43cff3f34fa6819', gotData);
+
+  var button = select('#submit');
+  button.mousePressed(weatherAsk);
+
+  input = select('#city');
+
+  //var url = api+city+'&appid=ca44a6aad9418c3af43cff3f34fa6819'+units;
+  //loadJSON(url, gotData);
 }
 
 function gotData(data){
   weather = data;
+  console.log(weather);
 }
 
+function weatherAsk(){
+  var url = api+input.value()+'&appid=ca44a6aad9418c3af43cff3f34fa6819'+units;
+  loadJSON(url, gotData);
+}
 
 function draw(){
   background(100);
